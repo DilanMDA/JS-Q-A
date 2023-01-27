f537f86c5fe2c7f49d340ca729abc7701759ca24
DilanMDA <akiladilan04@gmail.com>
2023-01-27 11:32:05 +0530

    bubbling up/ propagate parent element

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const items = document.querySelectorAll("li");
+const list = document.querySelector("ul");
 
 items.forEach((item) => {
   item.addEventListener("click", () => {
     console.log("item clicked");
   });
+});
+
+list.addEventListener("click", () => {
+  console.log("list clicked");
 });
